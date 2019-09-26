2bb2f3a0fb61564ea7805d550821ec617974b53d
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-09-26 10:52:44 +0200

    Add cors module

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
-const port = 5000
+const cors = require('cors')
 
 const app = express()
 
+const middleware = cors()
+
+app.use(middleware)
+
+const port = 5000
 
 app.get("/test", (req, res) => {
     res.send({
@@ -11,7 +15,13 @@ app.get("/test", (req, res) => {
     })
 })
 
-app.listen(() => {
+app.get("/test2", (req, res) => {
+    res.send({
+        message: 'Hello World'
+    })
+})
+
+app.listen(port, () => {
     console.log(`Started Listening on port ${port}`)
 })
 
